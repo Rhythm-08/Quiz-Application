ca2ebf1a2b88a0657bceefe7f1d6f2ba8b443451
Rhythm-08 <rhythmsharma808@gmail.com>
2023-05-14 22:14:33 +0530

    Quiz Questions setup

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +6,50 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  quizList:any[]=
+    [{
+      "question1": "What is the capital of the United States?",
+      "answer1" : "Washington, D.C."
+    },
+    {
+      "question2": "What is the capital of Canada?",
+      "answer2" : "Ottawa"
+    },{
+        "question3": "What is the capital of Mexico?",
+        "answer3" : "Mexico City"
+    },
+    {
+      "question4":"What is the capital of Brazil?",
+      "answer4" : "Brasilia"
+
+    },
+    {
+      "question5":"What is the capital of Argentina?",
+      "answer5" : "Buenos Aires"
+    },
+    {
+      "question6":"What is the capital of Chile?",
+      "answer6" : "Santiago"
+    },
+    {
+      "question7":"What is the capital of Peru?",
+      "answer7" : "Lima"
+
+    },
+    {
+      "question8":"What is the capital of Colombia?",
+      "answer8" : "Bogota"
+    },
+    {
+      "question9":"What is the capital of Venezuela?",
+      "answer9" : "Caracas"
+    },
+    {
+      "question10":"What is the capital of Ecuador?",
+      "answer10" : "Quito"
+    }
+
+  ]
+
 }
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from '../quiz.service';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit{
+  constructor(private quizService:QuizService){}
+quizArray:any[]=[];
+
+
+  ngOnInit(): void {
+    this.quizArray=this.quizService.quizList;
+    console.log(this.quizArray)
+  }
 
 }

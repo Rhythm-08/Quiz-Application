813d766d4bbcd6f0afee6c921ade171a4cc6001d
Rhythm-08 <rhythm.sharma@chicmic.co.in>
2023-05-14 21:37:57 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { QuestionComponent } from './question/question.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    QuestionComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,

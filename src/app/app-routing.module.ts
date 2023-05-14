3b0e8e4e4aeb632d632f8c90e8360c4e131f1955
Rhythm-08 <rhythm.sharma@chicmic.co.in>
2023-05-14 21:50:36 +0530

    feat: Nav-bar setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { QuestionComponent } from './question/question.component';
+import { ResultComponent } from './result/result.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'quiz',component:QuizComponent},
+  {path:'question',component:QuestionComponent},
+  {path:'result',component:ResultComponent},
+  {path:'',redirectTo:'quiz',pathMatch:'full'},
+  {path:'**',redirectTo:'quiz',pathMatch:'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quiz-Application';
+  isQuizActive:boolean = false;
+  isQuestionActive:boolean = false;
+  isResultActive:boolean = false;
+
+  clickResult(){
+    this.isResultActive = true;
+    this.isQuestionActive=false;
+    this.isQuizActive = false;
+
+
+  }
+clickQuiz(){
+  this.isQuizActive = true;
+  this.isQuestionActive=false;
+  this.isResultActive = false;
+
 }
+clickQuestion(){
+  this.isQuestionActive = true;
+  this.isQuizActive = false;
+  this.isResultActive = false;
+
+
+  }
+
+}
+
